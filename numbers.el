6ec0649e5aaefaada06c1b7bbf9d6a4543ca1b9c
Matt Teichman <teichman@uchicago.edu>
2023-03-17 10:58:35 -0500

    new version...

--- numbers.el
+++ numbers.el
@@ -1,11 +1,18 @@
 (defun matt-number-headers (&optional beg end)
   "TODO."
+  (interactive)
   (save-match-data
     (save-excursion
       (let ((num nil)
 	    (n 1)
-	    (end (set-marker (make-marker) (or end (point-max)))))
-	(setq beg (or beg (point-min)))
+	    (end
+             (set-marker (make-marker)
+                         (if (use-region-p)
+                             (region-end)
+                           (or end (point-max))))))
+	(setq beg (if (use-region-p)
+                      (region-beginning)
+                    (or beg (point-min))))
 	(goto-char beg)
 	;; TODO use org regexp
 	(while (re-search-forward (rx bol (+ "*")) end t)
